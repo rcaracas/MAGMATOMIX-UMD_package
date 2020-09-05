a75589782dd908a31cc9fc4645eef912ce7a4dff
L.Gilquin <lgilquin@free.fr>
2020-09-05 09:30:22 +0200

    included the gofr C extensions

--- UMD_package/gofrs_umd.py
+++ UMD_package/gofrs_umd.py
@@ -1,137 +1,226 @@
 ###
 #AUTHORS: RAZVAN CARACAS, ANAIS KOBSCH
+#MODIFICATIONS: LAURENT GILQUIN, 5 September 2020
 ###
 
-import sys,getopt,numpy,os.path,math
+import os
+import itertools
+import getopt
+import sys
+import numpy as np
 from . import crystallography as cr
 from . import umd_process as umdp
+from . import c_gofr
+from . import gpu_utils
 
-def print_gofrs(umdfile,MyCrystal,ndivx,discrete,normalization,maxlength,gofr):
-    """Normalize gofr, compute integral, create the new file gofrs.dat and write data in it """
-    newfile = umdfile[:-8] + '.gofr.dat' #name of the new file
-    nf = open(newfile,'w')
-    string = 'dist'+' '
-    for ii in range(MyCrystal.ntypat):
-        for jj in range(MyCrystal.ntypat):
-            string = string + str(MyCrystal.elements[ii])+'-'+MyCrystal.elements[jj]+'\t Int('+MyCrystal.elements[ii]+'-'+MyCrystal.elements[jj]+')\t'
-    string = string + '\n'
-    nf.write(string)
-    intgofr = [[0.0 for jatom in range(MyCrystal.ntypat)] for iatom in range (MyCrystal.ntypat)]
-    for kk in range(ndivx+1):
-        #print ('step no. ',kk)
-        smallr = float(kk)*discrete
-        bigr = smallr + discrete
-        volshell = 4.0 * numpy.pi * (bigr**3 - smallr**3) / 3.0
-        volcell = maxlength**3 #in case of non cubic cell, we'll have to use MyCrystal.cellvolume, and so import the rprimd in the read_umd
-        string = str(smallr+discrete/2)+' ' 
-        for iatom in range(MyCrystal.ntypat):
-            for jatom in range(MyCrystal.ntypat):
-                gofr[iatom][jatom][0] = 0 #we delete this element since it correspond to the distance = 0 (iatom with itself) 
-                gofr[iatom][jatom][kk] = float(gofr[iatom][jatom][kk]) * volcell / (volshell * MyCrystal.types[iatom] * MyCrystal.types[jatom] * normalization )
-                intgofr[iatom][jatom] += gofr[iatom][jatom][kk]*smallr**2*discrete*4*numpy.pi * MyCrystal.types[jatom]/volcell #cumulative integral of gofr in spherical coordinates 
-                string = string + str(gofr[iatom][jatom][kk]) + '\t' + str(intgofr[iatom][jatom]) + '\t'
-        string = string + '\n'
-        nf.write(string)
+
+def umd_pdist(X, coeff):
+    """
+    Interface with C wrapper to compute the umd distance, euclidean distance
+    with shift.
+    """
+    # correct X type if not double
+    if (X.dtype != np.double):
+        X = np.ascontiguousarray(X, dtype=np.double)
+    # initialize output
+    res = np.zeros(int(X.shape[0] * (X.shape[0] - 1)/2), dtype=np.double,
+                   order='C')
+    # call the CPython wrapper
+    c_gofr.umd_pdist_wrapper(X, res, coeff)
+    
+    return res
+
+
+    
+def print_gofrs(umdfile, MyCrystal, discrete, normalization, maxlength, gofr):
+    """
+    Normalize gofr, compute integral, create the new file gofrs.dat and write
+    data in it."""
+
+    # open output file
+    filename = umdfile[:-8] + '.gofr.dat'
+    nf = open(filename, 'w')
+    # write header
+    pairs = itertools.product(MyCrystal.elements, MyCrystal.elements)
+    string = ['dist ']
+    string.append("\t".join([elem[0]+'-'+elem[1]+'\t Int('+elem[0]+'-'+elem[1]+')' for elem in pairs]))
+    string.append('\n')
+    nf.write("".join(string))
     nf.close()
 
-
-def computeallgofr(MyCrystal,MySnapshot,discrete,maxlength,gofr):
-    """For every couple of atoms, compute the minimal distance betweens atoms and add 1 to the corresponding gofr[r,r+delta_r] slot """
-    for iatom in range (MyCrystal.natom):
-        for jatom in range(MyCrystal.natom):
-            #if you change this line to for jatom in range(iatom+1,MyCrystl.natom) you speed up everything by two. 
-            #just don't forget about the double counting
-            #compute distances along x, y and z between the two atoms
-            dx = MySnapshot.atoms[jatom].xcart[0] - MySnapshot.atoms[iatom].xcart[0]
-            dy = MySnapshot.atoms[jatom].xcart[1] - MySnapshot.atoms[iatom].xcart[1]
-            dz = MySnapshot.atoms[jatom].xcart[2] - MySnapshot.atoms[iatom].xcart[2]
-            #if these distances are too large, then we correct them by translating one of the two atoms of one unit cell
-            #if we want to work on a non cubic cell, then these corrections need to be changed
-            #if (dx < -maxlength/2): dx = dx + maxlength  #these two lines are replaced by the one below: if |dx|>acell/2 then int(...) gives ± 1 
-            #if (dx >  maxlength/2): dx = dx - maxlength
-            dx = dx-MyCrystal.acell[0]*int(dx/(0.5*MyCrystal.acell[0]))
-            dy = dy-MyCrystal.acell[1]*int(dy/(0.5*MyCrystal.acell[1]))
-            dz = dz-MyCrystal.acell[2]*int(dz/(0.5*MyCrystal.acell[2]))
-            d1 = math.sqrt(dx**2 + dy**2 + dz**2) 
-            if (d1 < maxlength/2): #if the distance between the two atoms is below half of the minimum distance of the unit cell, then we use it to compute the gofr
-                gofr[MyCrystal.typat[iatom]][MyCrystal.typat[jatom]][int(d1/discrete)] += 1
-    return(gofr)
+    # write data to file
+    types = np.array(MyCrystal.types, dtype=int, order='C')
+    discrete = np.double(discrete)
+    maxlength = np.double(maxlength)
+    normalization = np.double(normalization)
+    c_gofr.print_gofr_wrapper(gofr, types, MyCrystal.ntypat, maxlength, discrete,
+                              normalization, filename)
+    # empty return
+    return None
 
 
-def read_umd(umdfile,Nsteps,discrete,InitialStep):
+
+def computeallgofrs(MyCrystal, atoms_coords, discrete, maxlength, gofr):
+    """
+    For every pair of atoms, compute the minimal distance betweens atoms
+    and add 1 to the corresponding gofr[r, r + delta_r] slot.
+    """
+    
+    # check type arguments
+    coeffs = np.array(MyCrystal.acell, dtype=np.double, order='C')
+    types = np.array(MyCrystal.typat, dtype=int, order='C')
+    discrete = np.double(discrete)
+    maxlength = np.double(maxlength)
+     
+    # if GPU is available
+    if gpu_utils.get_gpu():
+        # compute the upper triangular part of the distance matrix
+        atoms_dist = gpu_utils.gpumd_pdist(atoms_coords, coeffs)
+        # lambda function to convert 2d (i,j) index to 1d k index with or without diagonal
+        def ij_to_k(M, cst):
+            return lambda x: int(M*(M+cst)/2-(M-min(x))*(M-min(x)+cst)/2+
+                             max(x)-min(x)+(cst-1)/2)
+        # get subsets of atoms per type
+        bounds = [MyCrystal.typat.index(typ) for typ in range(MyCrystal.ntypat)]
+        bounds.append(MyCrystal.natom)
+        # loop over subsets
+        for subset in itertools.combinations_with_replacement(range(MyCrystal.ntypat), 2):
+            # handle subset of same types
+            if subset[0] == subset[1]:
+                pairs = itertools.combinations(range(bounds[subset[0]], bounds[subset[0]+1]),2)
+            else:
+                pairs = itertools.product(range(bounds[subset[0]], bounds[subset[0]+1]),
+                                          range(bounds[subset[1]], bounds[subset[1]+1]))
+            dists = atoms_dist[list(map(ij_to_k(MyCrystal.natom,-1), pairs))]
+            bins = np.bincount((dists[dists < (maxlength/2)] / discrete).astype(np.int),
+                               minlength=gofr.shape[1])
+            # only count the pairs whose distances are lower than maxlength/2
+            gofr[(ij_to_k(MyCrystal.ntypat,1))(subset)][:] = bins
+    # or compute gofr directly through C wrapper
+    else:
+        c_gofr.compute_gofr_wrapper(atoms_coords, gofr, coeffs, types,
+                                    maxlength, discrete, MyCrystal.ntypat)
+    
+    # empty return
+    return None
+        
+
+
+def read_umd(umdfile, Nsteps, discrete, InitialStep):
     """Read umd file and store data into classes """
-    niter = 0
-    istep = 0
+
+    # convert str to int if possible
+    def str_to_int(x):
+        
+        try:
+            return int(x)
+        except ValueError:
+            return x
+
+    # initialize class storing the data
     MyCrystal = cr.Lattice()
-    with open(umdfile,'r') as ff:   #read the head and half of the 1st iter in order to get acell
-        while True:
+    # initialize header flag and the set of attributes to be reads
+    in_head = True
+    head_keys = set(["elements", "natom", "ntypat", "typat", "types"])
+    
+    # open umd file
+    with open(umdfile, 'r') as ff:
+
+        # get the head attributes
+        while in_head:
             line = ff.readline()
-            if not line: break
-            #print(line,len(line))
+            if not line:
+                break
+            # pass empty line
             if len(line) > 1:
-                line=line.strip()
-                entry=line.split()
-                if entry[0] == 'natom':
-                    MyCrystal.natom = int(entry[1])
-                    print('natom=',MyCrystal.natom)
-                    MyCrystal.typat = [0 for _ in range(MyCrystal.natom)]
-                if entry[0] == 'ntypat':
-                    MyCrystal.ntypat = int(entry[1])
-                    MyCrystal.types = [0 for _ in range(MyCrystal.ntypat)]
-                    MyCrystal.elements = ['X' for _ in range(MyCrystal.ntypat)]
-                if entry[0] == 'types':
-                    for ii in range(MyCrystal.ntypat):
-                        MyCrystal.types[ii]=int(entry[ii+1])
-                if entry[0] == 'elements':
-                    for ii in range(MyCrystal.ntypat):
-                        MyCrystal.elements[ii]=entry[ii+1]
-                if entry[0] == 'typat':
-                    for ii in range(MyCrystal.natom):
-                        MyCrystal.typat[ii] = int(entry[ii+1])
-                if entry[0] == 'acell':
-                    for i in range(3):
-                        MyCrystal.acell[i] = float(entry[i+1])
-                    maxlength = min(MyCrystal.acell[0],MyCrystal.acell[1],MyCrystal.acell[2])
-    ndivx = int(maxlength / (2*discrete))
-    print ('acell',MyCrystal.acell[0],'discrete',discrete,'ndivx ',ndivx)
-    gofr = [[[0 for kk in range(ndivx+1)] for jatom in range(MyCrystal.ntypat)] for iatom in range(MyCrystal.ntypat )]
-    with open(umdfile,'r') as ff:   #read the entire file to get the atomic positions
-        while True:
+                # format line
+                entry = (line.strip()).split()
+                # check if first element is an attribute
+                key = entry[0]
+                if key in head_keys:
+                    # remove the key
+                    head_keys.remove(key)
+                    values = [str_to_int(val) for val in entry[1:]]
+                    if len(values) == 1:
+                        setattr(MyCrystal, key, values[0])
+                    else:
+                        setattr(MyCrystal, key, values)
+                    # check if set is empty
+                    in_head = len(head_keys) != 0
+        
+        # read the first acell line
+        while key != "acell":
             line = ff.readline()
-            if not line: break
-            #print(line,len(line))
             if len(line) > 1:
-                line=line.strip()
-                entry=line.split()          
+                key = (line.strip()).split()[0]
+        entry = (line.strip()).split()
+        values = [float(val) for val in entry[1:-1]]
+        MyCrystal.acell = values
+        
+        # initialize gofr structure
+        maxlength = min(MyCrystal.acell[:2])
+        ndivx = int(maxlength / (2 * discrete))
+        print('acell', MyCrystal.acell[0], 'discrete', discrete, 
+              'ndivx ', ndivx)
+        kvals = int(MyCrystal.ntypat * (MyCrystal.ntypat + 1) / 2)
+        gofr = np.zeros((kvals, ndivx+1), dtype=np.intp, order='C')
+        
+        # get the atoms coordinates and calculate gofr for each time step
+        niter = 0
+        istep = 0
+        while not in_head:
+            
+            line = ff.readline()
+            if not line:
+                break
+            # pass empty line
+            if len(line) > 1:
+                entry = (line.strip()).split()       
                 if entry[0] == 'atoms:':
                     istep += 1
-                    #print('current iteration no.',istep)
-                    MySnapshot = cr.Lattice()
-                    MySnapshot.atoms = [cr.Atom() for _ in range(MyCrystal.natom)]
-                    for iatom in range(MyCrystal.natom):
-                        line = ff.readline()
-                        line=line.strip()
-                        entry=line.split()
-                        for jj in range(3):
-                            MySnapshot.atoms[iatom].xcart[jj]=float(entry[jj+3])
-                        #print(MySnapshot.atoms[iatom].xcart[0])
+                    # start computing from InitialStep
                     if istep > InitialStep:
-                        if niter % Nsteps == 0: #if the remainder of niter/Nsteps is 0, then I compute the gofr
-                            #print (' at step ',istep,' I am calling gofr ')
-                            gofr = computeallgofr(MyCrystal,MySnapshot,discrete,maxlength,gofr)
+                        # compute gofr only every Nsteps
+                        if niter % Nsteps == 0:
+                            # instantiate array to store atoms coordinates
+                            atoms_coords = np.empty((MyCrystal.natom, 3), order='C',
+                                                    dtype=np.double)
+                            # read coordinates and fill the structure
+                            for iatom in range(MyCrystal.natom):
+                                line = ff.readline()
+                                entry = (line.strip()).split()
+                                for jj in range(3):
+                                    atoms_coords[iatom][jj] = np.double(entry[jj+3])
+                            # compute the gofr every Nsteps
+                            computeallgofrs(MyCrystal, atoms_coords, discrete,
+                                            maxlength, gofr)
+                        else:
+                            # skip exactly natom lines to speed up the process
+                            for _ in range(MyCrystal.natom):
+                                next(ff)
                         niter += 1
-    normalization = niter / Nsteps  #number of time steps actually used in the calculation of gofr
-    print_gofrs(umdfile,MyCrystal,ndivx,discrete,normalization,maxlength,gofr)
-        
+                    else:
+                        # skip exactly natom lines to speed up the process
+                          for _ in range(MyCrystal.natom):
+                              next(ff)
+
+    # number of time steps actually used in the calculation of gofr
+    normalization = niter / Nsteps
+    print_gofrs(umdfile, MyCrystal, discrete, normalization, maxlength, gofr)
+    
+    # empty return
+    return None
+
+
 
 def main(argv):
     umdp.headerumd()
-    umdfile='output.umd.dat'
+    umdfile = 'output.umd.dat'
     Nsteps = 1
     discrete = 0.01            #delta_r  = width of bins in histogram
     InitialStep = 0            #in case we want to skip additional timesteps
     try:
-        opts, arg = getopt.getopt(argv,"hf:s:d:i:",["fumdfile","Sampling_Frequency","ddiscrete","iInitialStep"])
+        opts, arg = getopt.getopt(argv,"hf:s:d:i:", ["fumdfile", "Sampling_Frequency", "ddiscrete", "iInitialStep"])
     except getopt.GetoptError:
         print ('gofrs_umd.py -f <umdfile>  -s <No.steps>  -d <discretization.interval> -i <InitialStep>')
         sys.exit(2)
@@ -156,7 +245,7 @@ def main(argv):
             InitialStep = int(arg)
                 #print('I will skip  ',initial,' timesteps. ')
     if (os.path.isfile(umdfile)): 
-        read_umd(umdfile,Nsteps,discrete,InitialStep)
+        read_umd(umdfile, Nsteps, discrete, InitialStep)
     else:
         print ('the umd file ',umdfile,' does not exist')
         sys.exit()
